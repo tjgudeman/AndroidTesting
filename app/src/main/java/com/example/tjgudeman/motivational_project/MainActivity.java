148e766cbd0457c43d7aebc7ec9c4098be95a061
TJ Gudeman <trevor.gudeman@nwciowa.edu>
2016-08-24 10:59:38 -0500

    Added editable textfield and button. Removed text view and used a Spanable string instead.

--- app/src/main/java/com/example/tjgudeman/motivational_project/MainActivity.java
+++ app/src/main/java/com/example/tjgudeman/motivational_project/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.tjgudeman.motivational_project;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.SpannableString;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,6 +24,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+// Textbox number 1 which welcomes the user to the app
+        TextView text = (TextView) findViewById(R.id.text);
+        SpannableString content = new SpannableString("Content");
+        String textBox = "Welcome to my Tester App!";
+        String textBox2= "Please enter your name below.";
+
+        // Way to change format of textbox number 1 in a dynamic way
+        SpannableString spanString = new SpannableString(textBox + "\n\n" + textBox2);
+        spanString.setSpan(new UnderlineSpan(), 0, textBox.length(), 0);
+        spanString.setSpan(new StyleSpan(Typeface.BOLD), 0, spanString.length(), 0);
+        text.setText(spanString);
+
+
+ //       TextView text2 = (TextView) findViewById(R.id.text2);
+//        String textBox2= "Please enter your name below.";
+
+//        SpannableString spanString2 = new SpannableString("Please"); //Hardcoded because Android Studio was giving me problems
+//        spanString.setSpan(new UnderlineSpan(), 0, spanString2.length(), 0);
+//        spanString.setSpan(new StyleSpan(Typeface.BOLD), 0, spanString2.length(), 0);
+//        text2.setText(spanString2);
+
+
+
+        EditText editText = (EditText) findViewById(R.id.username);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                boolean worked = false;
+                if(actionId == EditorInfo.IME_ACTION_SEND){
+                    //sendMessage();
+                    worked = true;
+                }
+                return worked;
+            }
+        });
+
+
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
