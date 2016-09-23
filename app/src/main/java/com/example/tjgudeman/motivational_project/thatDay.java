860ca2aee40befbd687292bb1759067a6b1cc3c6
TJ Gudeman <trevor.gudeman@nwciowa.edu>
2016-09-22 22:46:27 -0500

    Made Results page look better and made it scrollabel. Also customized home button image.

--- app/src/main/java/com/example/tjgudeman/motivational_project/thatDay.java
+++ app/src/main/java/com/example/tjgudeman/motivational_project/thatDay.java
@@ -1,6 +1,7 @@
 package com.example.tjgudeman.motivational_project;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -29,6 +30,9 @@ public class thatDay extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Change Icon of FAB
+        fab.setImageDrawable(getResources().getDrawable(R.drawable.home));
 
 
         Bundle extras = getIntent().getExtras();
